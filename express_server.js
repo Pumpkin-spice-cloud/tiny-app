7517e54ad781c8597a9d3a04ddd7a40ad99c1051
Pumpkin-spice-cloud <alan_mo@live.com>
2019-12-11 21:40:26 +0000

    added cookies functionality which allows server to identify users

--- express_server.js
+++ express_server.js
@@ -2,15 +2,31 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+let cookieParser = require('cookie-parser');
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
+app.post("/login", (req,res) => {
+
+  const {username} = req.body;
+  //console.log(username);
+  res.cookie('username', username);
+  res.redirect("/urls");
+
+
+});
 app.post(`/urls/:shortURL/delete`, (req, res) => {
 
 
@@ -31,7 +47,11 @@ app.post(`/urls/:shortURL/edit`, (req, res) => {
 })
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies.username
+    
+  };
+  res.render("urls_new", templateVars);
 });
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
@@ -69,11 +89,12 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies.username };
+  console.log(templateVars.username);
   res.render("urls_index", templateVars);
 });
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies.username };
   res.render("urls_show", templateVars);
 });
 
